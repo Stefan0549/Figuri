1cc658ebc7758a0a29613c11992411bcd2be59d2
Stefan0549 <120709023+Stefan0549@users.noreply.github.com>
2023-05-04 09:43:08 +0300

    Add files via upload

---
+++ Figuri/Pravougalnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuri
+{
+    internal class Pravougalnik:Shape
+    {
+		public Pravougalnik(int lenght, int width, string name):base(name)
+		{
+			this.lenght=lenght;
+			this.width = width;
+		}
+
+		private int lenght;
+		private int width;
+
+		public int Width
+		{
+			get { return width; }
+			set { width = value; }
+		}
+
+		public int Lenght
+		{
+			get { return lenght; }
+			set { lenght = value; }
+		}
+
+	}
+}
